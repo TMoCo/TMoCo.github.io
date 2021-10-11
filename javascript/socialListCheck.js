7eebab947fa9e9940526f2b96ba720bc92288eca
TMoCo <tommy.moreno.cooper@gmail.com>
2021-10-11 09:51:36 +0200

    move script to separate file

---
+++ javascript/socialListCheck.js
@@ -0,0 +1,4 @@
+if(window.getComputedStyle(getElementById("list")).style.height == 0) {
+    // show backup list if list isnt visible
+    document.getElementById("backup list").style.height = "auto";
+}
