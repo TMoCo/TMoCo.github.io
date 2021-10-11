353055ef862a9569da30096bd0763a4543441095
TMoCo <tommy.moreno.cooper@gmail.com>
2021-10-11 10:10:30 +0200

    removed js

--- javascript/socialListCheck.js
+++ javascript/socialListCheck.js
@@ -1,4 +1,4 @@
-if(window.getComputedStyle(getElementById("list")).style.height == 0) {
+if(document.getElementById("list").offsetHeight == 0) {
     // show backup list if list isnt visible
     document.getElementById("backup list").style.height = "auto";
 }
